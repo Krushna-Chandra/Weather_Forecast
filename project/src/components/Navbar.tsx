a04738c8c78353a7b903442df831fcc2b1fe1cdd
Krushna Chandra Bindhani <kcbindhani90@gmail.com>
2025-08-04 21:59:15 +0530

    commit

--- project/src/components/Navbar.tsx
+++ project/src/components/Navbar.tsx
@@ -22,7 +22,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link to="/" className="flex items-center">
-              <img src="/logo.jpg" alt="Logo" className="w-8 h-8 mr-2" />
+              <img src="/logo.png" alt="Logo" className="w-8 h-8 bg-white rounded-full p-1" />
               <span className="text-xl font-bold text-gray-800 dark:text-white">Weather App</span>
             </Link>
           </div>
